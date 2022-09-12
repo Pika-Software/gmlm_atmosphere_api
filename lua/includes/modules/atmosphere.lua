c0734948eb82b3570a2b3c555fe6eedbb5841a82
PrikolMen <olegis019@gmail.com>
2022-09-12 18:55:34 +0400

    Update atmosphere.lua

--- lua/includes/modules/atmosphere.lua
+++ lua/includes/modules/atmosphere.lua
@@ -2,25 +2,41 @@ module( "atmosphere", package.seeall )
 
 if (CLIENT) then
 
-    function Run( name, ... )
-        if isstring( name ) then
-            RunConsoleCommand( "menu_api", name, ... )
+    local isstring = isstring
+
+    do
+        local RunConsoleCommand = RunConsoleCommand
+
+        function Run( name, ... )
+            if isstring( name ) then
+                RunConsoleCommand( "menu_api", name, ... )
+            end
         end
     end
 
-    net.Receive("atmosphere", function()
-        local name = net.ReadString()
-        if isstring( name ) and (name ~= "") then
-            local args = net.ReadTable()
-            if istable( args ) then
-                if (#args > 0) then
-                    Run( name, unpack( args ) )
-                else
-                    Run( name )
+    do
+
+        local net_ReadString = net.ReadString
+        local net_ReadTable = net.ReadTable
+        local net_Receive = net.Receive
+        local istable = istable
+        local unpack = unpack
+
+        net_Receive("atmosphere", function()
+            local name = net_ReadString()
+            if isstring( name ) and (name ~= "") then
+                local args = net_ReadTable()
+                if istable( args ) then
+                    if (#args > 0) then
+                        Run( name, unpack( args ) )
+                    else
+                        Run( name )
+                    end
                 end
             end
-        end
-    end)
+        end)
+
+    end
 
 end
 
@@ -28,18 +44,24 @@ if (SERVER) then
 
     util.AddNetworkString( "atmosphere" )
 
+    local net_WriteString = net.WriteString
+    local net_WriteTable = net.WriteTable
+    local net_Broadcast = net.Broadcast
+    local net_Start = net.Start
+    local net_Send = net.Send
+
     function Send( ply, name, ... )
-        net.Start( "atmosphere" )
-            net.WriteString( name )
-            net.WriteTable( {...} )
-        net.Send( ply )
+        net_Start( "atmosphere" )
+            net_WriteString( name )
+            net_WriteTable( {...} )
+        net_Send( ply )
     end
 
     function Broadcast( name, ... )
-        net.Start( "atmosphere" )
-            net.WriteString( name )
-            net.WriteTable( {...} )
-        net.Broadcast()
+        net_Start( "atmosphere" )
+            net_WriteString( name )
+            net_WriteTable( {...} )
+        net_Broadcast()
     end
 
 end
