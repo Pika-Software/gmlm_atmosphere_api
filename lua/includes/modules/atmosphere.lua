7b476f43ea4994dc36eb7aaea30d68c8d2539f03
PrikolMen <olegis019@gmail.com>
2022-09-18 00:37:42 +0400

    new api version

--- lua/includes/modules/atmosphere.lua
+++ lua/includes/modules/atmosphere.lua
@@ -2,44 +2,79 @@ module( "atmosphere", package.seeall )
 
 if (CLIENT) then
 
-    local isstring = isstring
-
+    -- Menu -> Client API
     do
-        local RunConsoleCommand = RunConsoleCommand
 
-        function Run( name, ... )
-            if isstring( name ) then
-                RunConsoleCommand( "menu_api", name, ... )
-            end
+        local apiCommands = {}
+        function SetFunc( name, func )
+            assert( isstring( name ), "Argument #1 must be string!" )
+            assert( isfunction( func ), "Argument #2 must be function!" )
+            apiCommands[ name ] = func
         end
-    end
 
-    do
-
-        local net_ReadString = net.ReadString
-        local net_ReadTable = net.ReadTable
-        local net_Receive = net.Receive
-        local istable = istable
-        local unpack = unpack
-
-        net_Receive("atmosphere", function()
-            local name = net_ReadString()
-            if isstring( name ) and (name ~= "") then
-                local args = net_ReadTable()
-                if istable( args ) then
-                    if (#args > 0) then
-                        Run( name, unpack( args ) )
-                    else
-                        Run( name )
-                    end
+        do
+            local isfunction = isfunction
+            function RunFunc( name, ... )
+                assert( isstring( name ), "Argument #1 must be string!" )
+                local func = apiCommands[ name ]
+                if isfunction( func ) then
+                    func( ... )
                 end
             end
-        end)
+        end
+
+        concommand.Add("atmosphere_api", function( ply, cmd, args )
+            RunFunc( unpack( args ) )
+        end, nil, "Menu -> Client API", FCVAR_DONTRECORD )
+
+    end
+
+    -- Client -> Menu API
+    do
+
+        local isstring = isstring
+
+        -- Client -> Menu API
+        do
+            local RunConsoleCommand = RunConsoleCommand
+
+            function Menu( name, ... )
+                if isstring( name ) then
+                    RunConsoleCommand( "menu_api", name, ... )
+                end
+            end
+        end
+
+        -- Server -> Client -> Menu API
+        do
+
+            local net_ReadString = net.ReadString
+            local net_ReadTable = net.ReadTable
+            local net_Receive = net.Receive
+            local istable = istable
+            local unpack = unpack
+
+            net_Receive("atmosphere", function()
+                local name = net_ReadString()
+                if isstring( name ) and (name ~= "") then
+                    local args = net_ReadTable()
+                    if istable( args ) then
+                        if (#args > 0) then
+                            Menu( name, unpack( args ) )
+                        else
+                            Menu( name )
+                        end
+                    end
+                end
+            end)
+
+        end
 
     end
 
 end
 
+-- Server -> Client API
 if (SERVER) then
 
     util.AddNetworkString( "atmosphere" )
